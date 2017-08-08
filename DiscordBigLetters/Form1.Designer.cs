9f5be7aced8239e84d56c32b7883a89396aacd2a
Merk <Merk@ERROR>
2017-08-07 19:46:58 -0500

    Add project files.

---
+++ DiscordBigLetters/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace DiscordBigLetters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(312, 314);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.button1.Location = new System.Drawing.Point(330, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.Maroon;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.richTextBox2.Location = new System.Drawing.Point(411, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(312, 314);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox2_KeyPress);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.checkBox1.Location = new System.Drawing.Point(337, 67);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Space?";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.checkBox2.Location = new System.Drawing.Point(328, 90);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(83, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Puncuation?";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.button2.Location = new System.Drawing.Point(330, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Goldenrod;
+            this.button3.Location = new System.Drawing.Point(330, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Copy";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Goldenrod;
+            this.button4.Location = new System.Drawing.Point(735, 334);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Copy";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DiscordBigLetters.Properties.Resources.discord;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(740, 339);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Discord Blue Text";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ DiscordBigLetters/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscordBigLetters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            index = 0;
+            richTextBox2.Text = "------------ New Message ------------\n";
+            if (richTextBox1.Text.Length >= 10000)
+            {
+                MessageBox.Show("Enter Text Less Than 10000 characters");
+            }
+            else
+            {
+                int counter = 0;
+                for (int i = 0; i < richTextBox1.Text.Length; i++)
+                {
+                    counter += 1;
+                    if(counter >= 9)
+                    {
+                        richTextBox2.Text += "\n\n------------ New Message ------------\n";
+                        counter = 0;
+                    }
+                    if (richTextBox1.Text[i].ToString() != " ")
+                    {
+                        if (!Char.IsLetter(richTextBox1.Text[i]))
+                        {
+                            if (checkBox2.Checked)
+                            {
+                                richTextBox2.Text += richTextBox1.Text[i];
+                            }else
+                            {
+                                counter -= 1;
+                            }
+                        }
+                        else
+                        {
+                            char index = richTextBox1.Text[i];
+                            string lowerIndex = index.ToString().ToLower();
+                            richTextBox2.Text += String.Format(":regional_indicator_{0}: ", lowerIndex);
+                        }                       
+                    }
+                    else
+                    {
+                        if (checkBox1.Checked)
+                        {
+                            richTextBox2.Text += " ";
+                        }
+                    }
+                }
+            }
+        }
+        int index = -1;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (index != -1)
+            {
+                string text = richTextBox2.Text;
+                string[] splits = text.Split(new string[] { "\n\n------------ New Message ------------\n" }, StringSplitOptions.RemoveEmptyEntries);
+                if (index <= splits.Length)
+                {
+                    if (index == 0)
+                    {
+                        richTextBox2.Select(index + 38, splits[index].Length - 38);
+                    }
+                    else
+                    {
+                        int c = 0;
+                        for (int x = 0; x < index; x++)
+                        {
+                            c += splits[x].Length;
+                            c += 40;
+                        }
+                        richTextBox2.Select(c, splits[index].Length);
+                    }
+                    richTextBox2.Focus();
+
+                    index += 1;
+                }
+                else
+                {
+                    MessageBox.Show("You can't go any further");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(richTextBox2.SelectedText);
+            }
+            catch { }
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+        }
+
+        private void richTextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        int switchs = 0;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = DiscordBigLetters.Properties.Resources.ripbear;
+            ImageLayout[] layouts = { ImageLayout.Center, ImageLayout.Stretch, ImageLayout.Tile, ImageLayout.Zoom };
+            switch (switchs)
+            {
+                case 0:
+                    this.BackgroundImageLayout = layouts[0];
+                    break;
+                case 1:
+                    this.BackgroundImageLayout = layouts[1];
+                    break;
+                case 2:
+                    this.BackgroundImageLayout = layouts[2];
+                    break;
+                case 3:
+                    this.BackgroundImageLayout = layouts[3];
+                    break;
+            }
+            if(switchs >= 3)
+            {
+                switchs = 0;
+            }
+            else
+            {
+                switchs += 1;
+            }
+        }
+    }
+}
